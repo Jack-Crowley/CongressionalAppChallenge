203801d214f4d2a2b6ac107048c89ca7df98d219
Jack-Crowley <jcrowley.jjc@gmail.com>
2023-11-01 00:17:00 -0400

    db with auth

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const fs = require("fs");
 const db = require('./db/db_connection');
+const { auth, requiresAuth } = require('express-openid-connect');
+require("dotenv").config()
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.AUTH_SECRET,
+    baseURL: 'http://localhost:3000',
+    clientID: 'xFcg2M5WA23acnOfo8OvdWSA6BaED0GH',
+    issuerBaseURL: 'https://dev-vs0v5722j8irc2o0.us.auth0.com'
+};
+
 
 const app = express();
 const PORT = 3000;
+
+app.use(auth(config));
 
 app.use(bodyParser.urlencoded({ extended: false, limit: '10mb' }));
 app.use(bodyParser.json());
@@ -23,10 +37,17 @@ app.get("/opportunities", async (req, res) => {
         if (error)
             res.status(500).send(error);
         else
-        res.render('opportunities', {results:results})    
+            res.render('opportunities', { results: results })
     });
-    
 });
+
+app.get('/logintest', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  });
 
 app.get("/", async (req, res) => {
     res.render('index')
